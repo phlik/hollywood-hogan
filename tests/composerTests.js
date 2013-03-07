9f4f4d0dbdd694d4a315a425e08a83905b25ea79
Erin Houston <erinhouston@gmail.com>
2013-03-07 07:56:38 -0700

    started work on making the composer useable

--- tests/composerTests.js
+++ tests/composerTests.js
@@ -1,2 +1,21 @@
+var assert = require("assert");
+describe("operation ", function(){
+    var composer;
+    var worker;
+    beforeEach(function(){
+        composer = require("../lib/composer.js");
+        worker = composer.create({nameSpace :"", baseFolderPath : ""});
+    });
+    it("should contain an create function", function(){
+        assert.equal('function', typeof composer.create);
+    });
 
-var composer = require("../lib/composer.js");
+    describe("the created object",function(){
+        it("should have a function called compileTemplates", function(){
+            assert.equal('function', typeof worker.compileTemplates);
+        });
+    });
+
+
+
+});
