83856160cf2931ca96ec8dcdfa036fb419dedfd1
Erin Houston <erinhouston@gmail.com>
2013-03-07 19:48:56 -0700

    some basic changes to the index

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-/**
- * Created with JetBrains WebStorm.
- * User: ehouston
- * Date: 2/21/13
- * Time: 8:35 AM
- * To change this template use File | Settings | File Templates.
- */
-var composer
+;!function(module){
+    var composer = require("./lib/composer.js");
+
+    var ret = {
+        compileTemplates:function(options){
+
+        }
+    };
+    module.exports = ret;
+
+}(typeof process !== 'undefined' && typeof process.title !== 'undefined' && typeof exports !== 'undefined' ? module : window);
