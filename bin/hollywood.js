ccba5b9d98c4c1e37bfcbc0c74640f85d970c586
Erin Houston <erinhouston@gmail.com>
2013-03-27 16:33:00 -0600

    hollywood now takes command lines
    wood comand line is nice now

--- bin/hollywood.js
+++ bin/hollywood.js
@@ -1,11 +1,9 @@
-
-
-// dependencies
 var wood  = require('../index.js');
 var nopt   = require('nopt');
 
 function printHelp(){
-    console.log('bla bla bla');
+    var help = "'-n' or '--namespace' sets the name space.\n'-d' or '--directory' sets the directory to run under.\n'-f' or '--filter' sets the filter \n'-h' or '--help' prints this message\n'-v' or '--version' will print the version.\n";
+    console.log(help);
 }
 
 function printVersion(){
@@ -34,28 +32,24 @@ options = nopt(options, shortHand);
 
 if(options.version){
     printVersion();
-}
-
-if(options.help){
+} else if(options.help || options.argv.cooked.length === 0){
     printHelp();
+} else {
+    var opt = {
+        nameSpace :options.namespace || "templates",
+        baseFolderPath : options.directory || "./",
+        filter: options.filter || '.*'
+    };
+    wood.compileTemplates(opt, function(err, data){
+        if(err && err.length > 0){
+            console.log('errors', err);
+        }
+        if(data){
+            console.log('');
+            console.log(data);
+        }
+    });
 }
-
-
-var opt = {
-    nameSpace :options.namespace || "templates",
-    baseFolderPath : options.directory || "./",
-    filter: options.filter || '.*'
-};
-
-wood.compileTemplates(opt, function(err, data){
-    if(err && err.length > 0){
-        console.log('errors', err);
-    }
-    if(data){
-        console.log('');
-        console.log(data);
-    }
-});
 
 
 
